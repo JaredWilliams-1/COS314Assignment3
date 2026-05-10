0817cdf0711c60811ea00685f7adbb391d0f7c25
JaredWilliams-1 <jaredluke3@gmail.com>
2026-05-10 19:16:49 +0200

    Added the string id

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String [] args){
 
-        
+        Node test1 = new Node(Node.Type.FUNCTION, "+");
+        test1.addLeft(new Node(Node.Type.TERMINAL, "2"));
+        test1.addRight(new Node(Node.Type.TERMINAL, "4"));
+
+        System.out.println(test1.TreeToString());
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,14 @@
 public class Node {
-    public String type;
+    enum Type {
+        FUNCTION,
+        TERMINAL
+    }
+    public Type type;
     public String value;
     public Node left;
     public Node right;
 
-    public Node(String type, String value){
+    public Node(Type type, String value){
         // type is whether it is function or terminal
         // value is the symbol (will obviously differ depending on whether it is terminal or terminal)
         this.type = type;
@@ -12,6 +16,19 @@ public class Node {
         this.left = null;
         this.right = null;
     }
+
+    //This is what is going to turn the tree into a unique String
+    //so you just use it by going into one of them GP functions and saying String uniqueString = root.TreeToString
+	public String TreeToString(){
+		return ("root: " + TreeToStringHelper(this));
+	}
+
+	private String TreeToStringHelper(Node curr){
+		if (curr == null){
+			return "null";
+		}
+		return (curr.value + "(L:(" + TreeToStringHelper(curr.left) + ")" + "R:(" + TreeToStringHelper(curr.right) +"))");
+	}
 
     public void addLeft(Node leftNode){
         this.left = leftNode;
